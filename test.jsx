03e499a631c838715c6a4a657e3f10eca8a9f93d
PrahoLama <148485526+PrahoLama@users.noreply.github.com>
2024-02-19 21:33:56 +0200

    Update test.jsx

--- test.jsx
+++ test.jsx
@@ -1,6 +1,16 @@
-function scrollToSection() {
-    var section2 = document.getElementById('Aranjare');
-    section2.scrollIntoView({
-        behavior: 'smooth'
-    });
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); 
 }
+showSlides(slideIndex);
+
